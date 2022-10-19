4b9d5e78d9272fb888e999cfbdcca724f3d639c1
mrRoleEx <115638496+mrRoleEx@users.noreply.github.com>
2022-10-19 20:57:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from dataclasses import replace
 import re
 
 import requests
 from bs4 import BeautifulSoup
 from pyrogram import Client, filters
 from pyrogram.client import Client
+from telegram import InputContactMessageContent
 
 # text = input('type movie name:- ')
 # url = 'https://vegamovies.baby/?s=' + text
@@ -46,12 +48,31 @@ async def getmovie(url):
     res = requests.get(url2)
     html2 = res.content.decode()
     soup2 = BeautifulSoup(html2,'html.parser')
-    #link2 = soup2.find_all('a.href')
+    imglin = soup2.find_all("div",attrs={"class":"movie-thumb"})
+    imig = []
+    for imag in imglin:
+        #print(imag.find('img').get('src'))
+        imig.append(imag.find('img').get('src'))
+        #print(imag.get('src'))
+        break
+    #print (imag.get('src'))
+    global imgres
+    imgres = imig
+    #imgres = imgrs.replace(" ","%")
+    #print(type(imgres))
+    #print(imgres)
+    str1 = ""
+    for iimglink in imgres:
+        str1 += iimglink
+        break
+    global fiinalimage2
+    fiinalimage = str1.replace(" ","%")
+    fiinalimage2 = fiinalimage
+    print(fiinalimage2)
+
     for linkk in soup2.find_all('a',
         attrs={'href': re.compile("^/page-downloading-page/")}):
         break
-        #print(linkk.get('href'))  
-
     reallink = linkk.get('href')
     t =  "".join(reallink)
     k = "https://filmy4wap.dev" + t
@@ -69,7 +90,6 @@ async def getmovie(url):
         break
     jaadu = linkk1.get('href')
     #print (jaadu)
-
     url4 = jaadu
     res3 = requests.get(url4)
     html4 = res3.content.decode()
@@ -81,7 +101,7 @@ async def getmovie(url):
         resse.append(linkk2.find('a').get('href'))
         #finlink = linkk2.find('a').get('href')
     finlink = resse  
-    print (finlink)
+    #print (finlink)
     return "\n".join(finlink)
 
     
@@ -97,8 +117,9 @@ async def sm(bot,message):
     url = 'https://filmy4wap.dev/site-1.html?to-search=' + movie_name
     resuult = await getmovie(url)
     await bot.send_message(message.chat.id,'Results of : '+ movie_name)
+    #await bot.send_photo(message.chat.id, fiinalimage2)
     await bot.send_message(message.chat.id,resuult)
-    await bot.send_message(message.chat.id,'By @iRoleEx')
+    await bot.send_message(message.chat.id,"bot by @iRoleEx")
 
 if __name__ == "__main__":
     bot.run()
