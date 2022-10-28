689a22962826fffbe52be9dd07b25ce1fd0c4f97
mrRoleEx <115638496+mrRoleEx@users.noreply.github.com>
2022-10-28 19:43:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import re
 from dataclasses import replace
 
@@ -8,12 +9,11 @@ from pyrogram.client import Client
 from pyrogram.types import InlineKeyboardButton as ikb
 from pyrogram.types import InlineKeyboardMarkup as ikm
 
-
 bot = Client("bot", bot_token="5744404637:AAFs-pq3UL34jqW1nti4eP8KOC8kf4Ncs_Q",
              api_id=1712043, api_hash="965c994b615e2644670ea106fd31daaf")
 
 
-async def getmovie(url):
+async def getmovie(url,url2):
     response = requests.get(url)
     html = response.content.decode()
     soup = BeautifulSoup(html, "html.parser")
@@ -24,6 +24,68 @@ async def getmovie(url):
         array.append(linnk.find("a").get("href"))
         if len(array) == 10:
             break
+    ResPons = requests.get(url2)
+    EchTiml = ResPons.content.decode()
+    Soup11 = BeautifulSoup(EchTiml, "html.parser")
+    ResultDekho = Soup11.find_all("div", attrs={"class": "title"})
+
+
+
+    
+    ResultArr = []
+  
+    
+
+
+    for RealResult in ResultDekho:
+        ResultArr.append(RealResult.find("a").get("href")) 
+        break
+    
+    strarray = ""
+    for realres in ResultArr:
+        strarray = realres
+        break
+    print(strarray)
+
+    global FinalResult
+    FinalResult = strarray
+    print (FinalResult)
+    
+    ResPonS7= requests.get(FinalResult)
+    EchTimm7 = ResPonS7.content.decode()
+    SoupPeelo = BeautifulSoup (EchTimm7,'html.parser')
+    ResultDekho7 = SoupPeelo.find_all("div" ,attrs={"class":"wp-content"})
+    picresult0 = SoupPeelo.find_all("div", attrs={"class": "poster"})
+
+    Arr8 = []
+    picarray = []
+    for pic in picresult0:
+            picarray.append(pic.find("img").get("src"))
+            break
+    strpic = ""
+    for respic in picarray:
+            strpic = respic
+            break
+        #print(strpic)
+    
+    global Finalpicture 
+    Finalpicture = strpic
+    for mainlink in ResultDekho7:
+        Arr8.append(mainlink.find("a").get("href"))
+        break
+    
+    #print(Arr8)
+    Sipen = ""
+    for sipen in Arr8:
+        Sipen += sipen
+        break
+    print(Sipen)
+    global FinalResultT
+    FinalResultT = Sipen
+    print (FinalResult)
+
+   
+
     result = array[:11]
     w = "https://filmy4wap.dev".join(result)
     g = "https://filmy4wap.dev" + w
@@ -51,6 +113,9 @@ async def getmovie(url):
     ):
         break
     jaadu = linkk1.get("href")
+    if jaadu == None:
+        return "No link found"
+    
     url4 = jaadu
     res3 = requests.get(url4)
     html4 = res3.content.decode()
@@ -103,8 +168,8 @@ def start(bot, message):
 
 
 def my_buttons(finlink,xxlink,n):
-    from pyrogram.types import InlineKeyboardMarkup as ikm
     from pyrogram.types import InlineKeyboardButton as ikb
+    from pyrogram.types import InlineKeyboardMarkup as ikm
     V=finlink
     name=xxlink
     
@@ -126,25 +191,30 @@ def my_buttons(finlink,xxlink,n):
       
       num+=1
     NV1.append(NV2)
-    print(ikm(NV1))
+    #print(ikm(NV1))
     return ikm(NV1)
 
 
 
 @bot.on_message(filters.command(["search"]))
 async def sm(bot, message):
-    await bot.send_message(message.chat.id, "searching.......")
+    await bot.send_message(message.chat.id, "searching....... \n Bot by - @IRoleEx")
     #button = ikb("Download", url=resse[1])
 
 #await bot.send_message(message.chat.id, "Download", reply_markup=ikm([[button]]))
     movie_name = " ".join(message.command[1:])
     url = "https://filmy4wap.dev/site-1.html?to-search=" + movie_name
-    resuult = await getmovie(url)
-    await bot.send_message(message.chat.id, "Results of : " + "" + movie_name + "")
+    url2 = "https://hdmovie91.com/?s=" + movie_name
+    resuult = await getmovie(url, url2)
+    
+    await bot.send_photo(message.chat.id,Finalpicture)
+    await bot.send_message(message.chat.id,"👇👇👇👇👇",reply_markup=ikm([[ikb(text="Download", url=FinalResultT)]]))
     # create a button using for loop for resse list
-
+    print (Finalpicture)
+    await bot.send_message(message.chat.id, "Results of : " + "" + movie_name + "")
     await bot.send_message(message.chat.id,"Result are :- ",reply_markup=resuult)
-    #await bot.send_message(message.chat.id,"results are:- ",resuult)
+    
+    #await bot.send_photo(message.chat.id, Finalpicture)
 
 
 if __name__ == "__main__":
